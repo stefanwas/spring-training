bffd7d600d30a29316e62c3117e67f6ce8fe5c4a
stefanwas <swtemp001@gmail.com>
2017-12-30 21:55:48 +0100

    added spring-security-jsp2 -training

---
+++ spring-security-jsp2-training/src/main/java/com/stefan/training/spring/security/jsp2/HelloController.java
@@ -0,0 +1,53 @@
+package com.stefan.training.spring.security.jsp2;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController
+{
+    @RequestMapping("/hello")
+    public ModelAndView hello()
+    {
+        String welcomeMessage = "Hello controller Welcome Message";
+        return new ModelAndView("hello", "welcomeMessage", welcomeMessage);
+    }
+    @RequestMapping("/admin")
+    public ModelAndView admin()
+    {
+        String welcomeMessage = "Welcome to Admin Page !!";
+        return new ModelAndView("admin", "welcomeMessage", welcomeMessage);
+    }
+
+    @RequestMapping("/error")
+    public String error(ModelMap model)
+    {
+        model.addAttribute("error", "true");
+        return "login";
+
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGet()
+    {
+        System.out.println("!!!!!!!!!!!!Inside login GET");
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPost()
+    {
+        System.out.println("!!!!!!!!!!!!!!Inside login POST");
+        return "login";
+    }
+    
+    @RequestMapping("/logout")
+    public String logout()
+    {
+        return "logout";
+    }
+}

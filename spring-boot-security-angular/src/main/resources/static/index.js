6765239d77a0e898228b48d797a41236a59c5253
stefanwas <swtemp001@gmail.com>
2018-01-13 02:09:38 +0100

    added spring-boot-security-angular - added ngRouter (started ui-router)

--- spring-boot-security-angular/src/main/resources/static/index.js
+++ spring-boot-security-angular/src/main/resources/static/index.js
@@ -1,12 +1,29 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('sample.app', ['ngResource'])
+angular.module('sample.app', ['ngResource', 'ui.router'])
 
-    // this is to instruct server to not send header to require basic authentication (Authenticate : Basic ...)
+    // this is to instruct server to NOT send header to require basic authentication (Authenticate : Basic ...)
     // it tells the server that the client is not a browser
-    .config(function($httpProvider) {
+    .config(function($httpProvider, $stateProvider) {
+
         $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+        var loginState = {
+            name: 'login',
+            url: '/login',
+            template: '<h3>Please log in:</h3>'
+        }
+
+        var welcomeState = {
+            name: 'welcome',
+            url: '/welcome',
+            template: '<h3>Welcome!</h3>'
+        }
+
+        $stateProvider.state(loginState);
+        $stateProvider.state(welcomeState);
+
     })
 
     .factory('DocumentService', ['$resource', function($resource) {
